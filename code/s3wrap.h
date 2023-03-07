f376568fe8afcc90989b0da47f6a1c402a7c645b
Isaac Khor <s.github@isaackhor.com>
2023-03-07 17:12:46 -0500

    Fix header files

--- code/s3wrap.h
+++ code/s3wrap.h
@@ -6,20 +6,24 @@
 #ifndef __S3WRAP_H__
 #define __S3WRAP_H__
 
+#include <libs3.h>
+#include <list>
+#include <string>
+
 #ifdef __cplusplus
 class s3_target {
-    std::string     host, bucket, access, secret;
-    S3Protocol      protocol;
-    
-public:
+    std::string host, bucket, access, secret;
+    S3Protocol protocol;
+
+  public:
     s3_target(const char *_host, const char *_bucket, const char *_access,
-	      const char *_secret, bool encrypted) :
-	host (_host), bucket (_bucket), access (_access), secret (_secret) {
-	protocol = encrypted ? S3ProtocolHTTPS : S3ProtocolHTTP;
+              const char *_secret, bool encrypted)
+        : host(_host), bucket(_bucket), access(_access), secret(_secret) {
+        protocol = encrypted ? S3ProtocolHTTPS : S3ProtocolHTTP;
     }
 
     S3Status s3_get(std::string key, ssize_t offset, ssize_t len,
-		     struct iovec *iov, int iov_cnt);
+                    struct iovec *iov, int iov_cnt);
     S3Status s3_delete(std::string key);
     S3Status s3_put(std::string key, struct iovec *iov, int iov_cnt);
     S3Status s3_head(std::string key, ssize_t *p_len);
@@ -27,16 +31,19 @@ public:
 };
 
 extern "C" void *s3_init(char *bucket, char *host, char *access, char *secret);
-extern "C" S3Status s3_read(void *_t, char *key, ssize_t offset, ssize_t len, struct iovec *iov, int iov_cnt);
-extern "C" S3Status s3_write(void *_t, char *key, struct iovec *iov, int iov_cnt);
+extern "C" S3Status s3_read(void *_t, char *key, ssize_t offset, ssize_t len,
+                            struct iovec *iov, int iov_cnt);
+extern "C" S3Status s3_write(void *_t, char *key, struct iovec *iov,
+                             int iov_cnt);
 extern "C" S3Status s3_len(void *_t, char *key, ssize_t *p_len);
 
 #else
 
-typedef void *s3_target;	// hack
+typedef void *s3_target; // hack
 
 void *s3_init(char *bucket, char *host, char *access, char *secret);
-S3Status s3_read(void *_t, char *key, ssize_t offset, ssize_t len, struct iovec *iov, int iov_cnt);
+S3Status s3_read(void *_t, char *key, ssize_t offset, ssize_t len,
+                 struct iovec *iov, int iov_cnt);
 S3Status s3_write(void *_t, char *key, struct iovec *iov, int iov_cnt);
 S3Status s3_len(void *_t, char *key, ssize_t *p_len);
 
