c05a0e3c301cd33ce323f02292215eca5d6efacb
Isaac Khor <s.github@isaackhor.com>
2023-05-01 19:13:17 -0400

    Add local data

--- measure/test_fs.bash
+++ measure/test_fs.bash
@@ -5,24 +5,24 @@ set -eu
 echo "==============================="
 echo "=== Fileserver workload ======="
 echo "==============================="
-#python3 ./delete_bucket_files.py
-filebench -f ./fileserver.f
+python3 ./delete_bucket_files.py
+sudo filebench -f ./fileserver.f
 
 echo "==============================="
 echo "=== OLTP workload       ======="
 echo "==============================="
-#python3 ./delete_bucket_files.py
-filebench -f ./oltp.f
+python3 ./delete_bucket_files.py
+sudo filebench -f ./oltp.f
 
 echo "==============================="
 echo "=== Random Write        ======="
 echo "==============================="
-#python3 ./delete_bucket_files.py
-filebench -f ./random_write.f
+python3 ./delete_bucket_files.py
+sudo filebench -f ./random_write.f
 
 echo "==============================="
 echo "=== Webserver workload  ======="
 echo "==============================="
-#python3 ./delete_bucket_files.py
-filebench -f ./webserver.f
+python3 ./delete_bucket_files.py
+sudo filebench -f ./webserver.f
 
