2ff9e611e8441bbd837cf8ee0696eb0094de39c7
Isaac Khor <s.github@isaackhor.com>
2023-05-04 15:31:11 -0400

    fix obfs script

--- measure/mount-obfs.bash
+++ measure/mount-obfs.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 set -u
+export S3_TEST_BUCKET_NAME='obfs-test'
 echo "Starting OBFS with backend $S3_HOSTNAME and bucket $S3_TEST_BUCKET_NAME"
 
 sudo umount -l /mnt/fsbench
-
-export S3_TEST_BUCKET_NAME='obfs-test'
 s3 -u create $S3_TEST_BUCKET_NAME
 python3 delete_bucket_files.py $S3_TEST_BUCKET_NAME
 
