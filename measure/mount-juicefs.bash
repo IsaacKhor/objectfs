78c4b615b8fddb028501d476b29e71121fde5766
Isaac Khor <s.github@isaackhor.com>
2023-05-04 22:35:00 +0000

    Add final remote results

--- measure/mount-juicefs.bash
+++ measure/mount-juicefs.bash
@@ -8,11 +8,18 @@ s3 -u create $S3_TEST_BUCKET_NAME
 python3 delete_bucket_files.py $S3_TEST_BUCKET_NAME
 rm -f myjfs.db
 
-juicefs format --storage minio \
-    --bucket http://$S3_HOSTNAME/$S3_TEST_BUCKET_NAME \
+#juicefs format --storage minio \
+#    --bucket http://$S3_HOSTNAME/$S3_TEST_BUCKET_NAME \
+#    --access-key $S3_ACCESS_KEY_ID \
+#    --secret-key $S3_SECRET_ACCESS_KEY \
+#    sqlite3://myjfs.db \
+#	myjfs
+
+juicefs format --storage s3 \
+    --bucket http://$S3_TEST_BUCKET_NAME.$S3_HOSTNAME \
     --access-key $S3_ACCESS_KEY_ID \
     --secret-key $S3_SECRET_ACCESS_KEY \
-    sqlite3://myjfs.db \
+    redis://10.1.0.6:6379 \
 	myjfs
 
-sudo juicefs mount sqlite3://myjfs.db /mnt/fsbench
+sudo juicefs mount redis://10.1.0.6:6379 /mnt/fsbench
