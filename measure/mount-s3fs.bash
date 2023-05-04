1575ac20c22ffab587c2d24e866f1f03b27e94d0
Isaac Khor <s.github@isaackhor.com>
2023-05-04 14:30:03 -0400

    Update s3fs to mount in foreground

--- measure/mount-s3fs.bash
+++ measure/mount-s3fs.bash
@@ -8,7 +8,7 @@ s3 -u create $S3_TEST_BUCKET_NAME
 python3 delete_bucket_files.py $S3_TEST_BUCKET_NAME
 
 echo "$S3_ACCESS_KEY_ID:$S3_SECRET_ACCESS_KEY" > ./passwd-s3fs
-sudo s3fs $S3_TEST_BUCKET_NAME /mnt/fsbench \
+sudo s3fs -f $S3_TEST_BUCKET_NAME /mnt/fsbench \
 	-o passwd_file=./passwd-s3fs \
 	-o url=http://$S3_HOSTNAME/ \
 	-o use_path_request_style
